d5b6776da1f7a6f6626e97957bf801b3f2ce06fb
sebasttan02 <105518954+sebasttan02@users.noreply.github.com>
2022-06-19 13:31:38 +0100

    Add files via upload

---
+++ high_and_low_pulse_frequency_detection.ino
@@ -0,0 +1,26 @@
+
+#include <Wire.h>
+
+const int pulsePin = 8; // Input signal connected to A1 of Metro M0 Express
+int pulseHigh; // Integer variable to capture High time of the incoming pulse
+int pulseLow; // Integer variable to capture Low time of the incoming pulse
+float pulseTotal; // Float variable to capture Total time of the incoming pulse
+float frequency; // Calculated Frequency
+
+void setup() {
+
+pinMode(pulsePin, INPUT);
+delay(5000);
+}
+
+void loop() {
+
+pulseHigh = pulseIn(pulsePin, HIGH);
+pulseLow = pulseIn(pulsePin, LOW);
+pulseTotal = pulseHigh + pulseLow; // Time period of the pulse in microseconds
+frequency = 1000000 / pulseTotal; // Frequency in Hertz (Hz)
+
+Serial.println(frequency);
+
+delay(500);
+}
